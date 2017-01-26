7485c2d8ad98d23f048e7f3e406a57c77a656466
2carrot84 <2carrot84@gmail.com>
2017-01-26 16:39:52 +0900

    17.01.26 2장 p.161 까지 진행

--- spring/src/main/java/springbook/user/dao/UserDaoTest.java
+++ spring/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,5 +1,7 @@
 package springbook.user.dao;
 
+import org.junit.Test;
+import org.junit.Assert;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -7,11 +9,33 @@ import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.CoreMatchers.is;
+
 /**
  * Created by 154910 on 2017-01-24.
  */
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+    @Test
+    public void addAndGet() throws ClassNotFoundException, SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+
+        Assert.assertThat(user2.getName(), is(user.getName()));
+        Assert.assertThat(user2.getPassword(), is(user.getPassword()));
+    }
+
+
+    protected static void main(String[] args) throws ClassNotFoundException, SQLException {
         /* TEST 클래스에서 관계 생성 책임(client) 분리
         ConnectionMaker connectionMaker = new DConnectionMaker();
         UserDao dao = new UserDao(connectionMaker);*/
@@ -20,11 +44,12 @@ public class UserDaoTest {
         // class bean 사용
         //ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
+        /** test 소스를 jUnit으로 변환
         // xml bean 설정 사용
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
-        /**
+
          * application context 이용한 객체 생성과 new 를 이용한 객체 생성의 차이 비교
          * application Context 를 이용할 경우 싱글톤 형태로 객체 생성(하나의 객체만 생성 후 동일한 객체 리턴)
          UserDao dao2 = context.getBean("userDao", UserDao.class);
@@ -37,7 +62,7 @@ public class UserDaoTest {
         UserDao dao4 = factory.userDao();
 
         System.out.println(dao3);
-        System.out.println(dao4);*/
+        System.out.println(dao4);
 
         User user = new User();
         user.setId("whiteship");
@@ -51,5 +76,6 @@ public class UserDaoTest {
         User user2 = dao.get(user.getId());
         System.out.println("name : " + user2.getName());
         System.out.println("id : " + user2.getId() + " 조회 성공");
+         */
     }
 }
