5052906cbcbc201d5714474ada1686b6ab012821
eguns <2carrot84@gmail.com>
2017-02-15 23:21:39 +0900

    17.02.15 home 용 설정 변경

--- spring/src/main/java/springbook/user/dao/jdbccontext/UserDaoJdbcContext.java
+++ spring/src/main/java/springbook/user/dao/jdbccontext/UserDaoJdbcContext.java
@@ -25,114 +25,18 @@ public class UserDaoJdbcContext {
     // 클라이언트
     public void deleteAll() throws ClassNotFoundException, SQLException {
         StatementStrategy starategy = new DeleteAllStatement(); // 전략 오브젝트 생성
-        jdbcContextWithStatementsStrategy(starategy);   // 컨텍스트 호출, 전략 오브젝트 전달
-        /*
-        3장 예외처리 전 소스
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();*/
-
-        /*
-        예외처리 적용 후
-        jdbc관련 컨텍스트를 분리
-        jdbcContextWithStatementsStrategy 메소드 분리
-
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            // 변하는 부분
-            //ps = c.prepareStatement("delete from users");
-            //ps = makeStatement(c);
-
-            // 변하는 부분을 전략(interface)로 분리하여, interface를 통해 deleteAll전략을 받아 실행
-            StatementStrategy starategy = new DeleteAllStatement();
-            ps = starategy.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        } catch (SQLException e) {
-//            throw e;
-            e.printStackTrace();
-        } finally {
-            if(ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("delete from users");
+                return ps;
             }
-
-            if(c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }*/
+        });   // 컨텍스트 호출, 전략 오브젝트 전달
     }
 
     public int getCount() throws ClassNotFoundException//, SQLException
     {
-        /*
-        3장 예외처리 전 소스
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
-
-        rs.close();
-        ps.close();
-        c.close();
-        */
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        int count = 0;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-            rs = ps.executeQuery();
-            rs.next();
-            count = rs.getInt(1);
-        } catch (SQLException e) {
-//            throw e;
-            e.printStackTrace();
-        } finally {
-            if(rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if(ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if(c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return count;
+        return 0;
     }
 
     // 클라이언트
@@ -153,87 +57,8 @@ public class UserDaoJdbcContext {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        /*
-        3장 예외처리 소스 전
-        Connection c = dataSource.getConnection();
 
-        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-        ps.setString(1, id);
 
-        ResultSet rs = ps.executeQuery();
-        *//* 쿼리 결과가 없을 경우 exception 발생
-        rs.next();
-
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));*//*
-
-        User user = null;
-
-        if(rs.next()) {
-            user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
-        }
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        if(user == null) throw new EmptyResultDataAccessException(1);
-
-        return user;*/
-
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        User user = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select * from users where id = ?");
-            ps.setString(1, id);
-            rs = ps.executeQuery();
-
-            if(rs.next()) {
-                user = new User();
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-            }
-        } catch (SQLException e) {
-//            throw e;
-            e.printStackTrace();
-        } finally {
-            if(rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if(ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if(c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        if(user == null) throw new EmptyResultDataAccessException(1);
-
-        return user;
+        return null;
     }
 }
