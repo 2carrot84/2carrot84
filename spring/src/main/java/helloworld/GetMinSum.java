1b0a8db329d6eca20590b2b2127bd18796081f04
2carrot84 <2carrot84@gmail.com>
2017-04-25 18:14:08 +0900

    '17.04.25 helloworld

--- spring/src/main/java/helloworld/GetMinSum.java
+++ spring/src/main/java/helloworld/GetMinSum.java
@@ -11,45 +11,35 @@ public class GetMinSum {
 		int []A = {1,2};
 		int []B = {3,4};
 		Assert.assertEquals(test.getMinSum(A, B), 10);
+		
+		int[] c = {8830, 5844, 8239, 777, 2961, 2422, 9397, 1302, 6107, 2860};
+		int[] d = {2349, 1764, 150, 5746, 4056, 5495, 7960, 2608, 7291, 5514};
+		Assert.assertEquals(test.getMinSum(c, d), 137604750);
 	}
 	
 	public int getMinSum(int[] A, int[] B) {
-		
+		int[][] temp = new int[A.length][B.length];
 		int answer = 0;
 		
-		for (int i = 0; i < A.length; i++) {
-			for (int j = 0; j < B.length; j++) {
-				System.out.println("A["+i+"]*B["+j+"] : " + A[i]*B[j]);
+		System.out.println(A.length + ", " + B.length);
+		
+		for (int i = 0; i < temp.length; i++) {
+			for (int j = 0; j < temp[0].length; j++) {
+				temp[i][j] = A[i]*B[j];
+				System.out.println("temp["+i+"]["+j+"] : " + temp[i][j]);
+				System.out.println("A["+i+"] : " + A[i]);
+				System.out.println("B["+j+"] : " + B[j]);
 			}
 		}
 
 		return answer;
-	}
-	
-	public int test(int[] a, int[] b, int indexA, int indexB) {
-		if(a.length == indexA && b.length == indexB) return a[indexA] * b[indexB];
-		
-		int min = 0;
-		
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < b.length; j++) {
-				if(i == indexA || j == indexB) {
-					continue;
-				}
-				
-				if(min > (a[i]*b[j])) {
-					min = a[i]*b[j];
-				}
-			}
-		}
-		
-		return min;
 	}
 
 	public static void main(String[] args) {
 		GetMinSum test = new GetMinSum();
 		int []A = {1,2,5};
 		int []B = {3,4,7};
+		
 		System.out.println(test.getMinSum(A,B));
 	}
 }
--- spring/src/main/java/study/LearningTest.java
+++ spring/src/main/java/study/LearningTest.java
@@ -1,10 +1,21 @@
 package study;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeMap;
+
 import org.junit.Assert;
 import org.junit.Test;
-import scala.Int;
-
-import java.util.*;
 
 /**
  * Created by eguns on 2017. 4. 23..
